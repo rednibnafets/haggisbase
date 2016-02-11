3fe5281bb37951bb1778de066fcdaabd5b8eac7e
s.binder <s.binder@Y350SDP01.ad.dundeeandangus.ac.uk>
2016-02-11 15:41:15 +0000

    02 - src.rdbms files changed.  Use Interface in main()

--- RDBMS/src/rdbms/RDBMS.java
+++ RDBMS/src/rdbms/RDBMS.java
@@ -1,6 +1,6 @@
-package rdbms;
+package Database.RDBMS.src.rdbms;
 
-import database.*;
+import Database.RDBMS.src.database.*;
 
 /**
  *
@@ -13,11 +13,22 @@ public class RDBMS {
         
         //Create databse and table
         Database d = new Database("Freight");
-        d.createTable("Parent Table", "Goods", 100);
+        //d.createTable("Parent Table", "Goods", 100);
         
+
+        //d.getTable("Goods").insert("Goods", "first_row");
         
+        TableInterface ti = TableFactory.createTable("Parent Table", "Goods");
+        
+        Row r = new Row("row_value") ;  
+        
+        ti.insert(r);
+        ti.select();
+                
+        //d.getTable("Goods").insert(r);
+                
         //Insert a row  
-        insert("Goods", "Apples");
+        //insert("Goods", "Apples");
     }
 
 }
